6d64c291eaf7a2307425c0aa927d4387a58b856b
hala214 <halasamer2042007@gmail.com>
2023-08-19 17:12:46 +0300

    add dashboard with charts and login page

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import oceanus_db
+
 app = Flask(__name__)
 
-project_name = "OCEANUS"
+db = oceanus_db.initialize_db()
+
+def valid_login(email, password):
+    auth_service = oceanus_db.intialize_auth()
+    try:
+        auth_service.sign_in_with_email_and_password(email,password)
+        return "succefully login"
+    except:
+        return "invalid email or password, try again"
 @app.route("/")
+@app.route("/dashboard")
+def dashboard():
+    return render_template("index.html")
 
-def index():
-   return render_template("index.html", title=project_name)
+@app.route("/charts")
+def charts():
+    return render_template("/chartjs.html")
 
-if __name__ == '__main__':
-   app.run(debug = True)
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    # error = None
+    if request.method == 'POST':
+        if valid_login(request.form['exampleInputEmail1'],
+                       request.form['exampleInputPassword1']):
+            return redirect("/dashboard")
+    
+    return render_template("login.html")
+
+if __name__ == "__name__":
+    app.run(debug=True)
+    # app.run()
---
+++ Dashboard/oceanus_db.py
@@ -0,0 +1,22 @@
+import pyrebase
+
+firebaseConfig = {
+    "apiKey": "AIzaSyBfTlFrewOca9OxubyqftNSqznTrJLWSlY",
+    "authDomain": "oceanus-purpose.firebaseapp.com",
+    "databaseURL": "https://oceanus-purpose-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "oceanus-purpose",
+    "storageBucket": "oceanus-purpose.appspot.com",
+    "messagingSenderId": "674501615782",
+    "appId": "1:674501615782:web:ee6243a17663fe196e5467",
+    "measurementId": "G-MDMB3N4508",
+    "databaseURL": "https://oceanus-purpose-default-rtdb.europe-west1.firebasedatabase.app/"}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+
+def initialize_db():
+    firebase_db = firebase.database()
+    return firebase_db
+
+def intialize_auth():
+    firebase_auth = firebase.auth()
+    return firebase_auth
