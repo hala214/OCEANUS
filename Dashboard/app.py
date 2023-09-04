80d35c4c58d849f194156e021c0da350119a7d50
MeqdadDev <meqdad.darweesh@gmail.com>
2023-09-04 12:40:23 +0300

    Oceanus v2.0 - Meqdad

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 import oceanus_db
 import rpi_mock
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -11,13 +12,14 @@ user_password = None
 
 login_visit = False
 
+
 def valid_login(email, password):
     global login_visit
     auth_service = oceanus_db.intialize_auth()
     try:
-        auth_service.sign_in_with_email_and_password(email,password)
+        auth_service.sign_in_with_email_and_password(email, password)
         login_visit = True
-        return email,password
+        return email, password
     except:
         return False
 
@@ -30,24 +32,38 @@ def dashboard():
     else:
         return redirect("/login")
 
+
 @app.route("/charts")
 def charts():
     return render_template("chartjs.html")
 
-@app.route('/get_data')
-def get_data():
-    data = {'Legal': 10,
-            'Illegal': 5}
-    return jsonify(data)
 
 @app.route("/tables")
 def tables():
     i = 0
+    firebase_items = db.child("Raspberry Pi Data").child("Ships").get()
     ships = []
-    allowed_ships = ["Bulk Carrier", "Passenger Ships"]
-    for i in range(5):
-        ships.append(rpi_mock.check_ships())
-    return render_template("table.html", ships=ships, allowed_ships=allowed_ships)
+    allowed_ships = ["Passenger Ship", "Fishing Ship"]
+
+    # for i in range(10):
+    for i in firebase_items.each():
+        print(i.val())
+        ship_item = i.val()
+        ship_name = ship_item["name"].capitalize()
+        ship_timestamp = ship_item["timestamp"]
+        # print("ship_name", ship_name)
+        # print("ship_timestamp", ship_timestamp)
+        # ship_name = str(i.val()["name"])
+        # name = str(i.val()["name"])
+        ship_timestamp = str(datetime.fromtimestamp(ship_timestamp))
+        date_object = datetime.strptime(ship_timestamp, "%Y-%m-%d %H:%M:%S.%f")
+        formatted_date = date_object.strftime(
+            "Date: %Y-%m-%d / Time: %H:%M:%S.%f")[:-4]
+        ships.append({"name": f"{ship_name} Ship", "time": formatted_date})
+
+    # print("ships::::", ships[:20])
+    return render_template("table.html", ships=ships[:20], allowed_ships=allowed_ships)
+
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -57,12 +73,12 @@ def login():
         user_email = request.form['exampleInputEmail1']
         user_password = request.form['exampleInputPassword1']
         global user_data
-        
+
         user_data = {"email": user_email}
 
         if valid_login(user_email, user_password):
             return redirect("/dashboard")
-    
+
     return render_template("login.html")
 
 
--- Dashboard/oceanus_db.py
+++ Dashboard/oceanus_db.py
@@ -11,13 +11,21 @@ firebaseConfig = {
     "measurementId": "G-MDMB3N4508",
     "databaseURL": "https://oceanus-purpose-default-rtdb.europe-west1.firebasedatabase.app/"}
 
-firebase=pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(firebaseConfig)
+
 
 def initialize_db():
     firebase_db = firebase.database()
     return firebase_db
 
+
 def intialize_auth():
     firebase_auth = firebase.auth()
     return firebase_auth
 
+
+def create_fake_data():
+    db = initialize_db()
+    data = {"name": "Palestinian", "timestamp": 1693486552.263934}
+    for i in range(10):
+        db.child("Raspberry Pi Data").child("Ships").child(data)
