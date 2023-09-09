22937019e86fc634957a6cad9a922faf5f8d1cb9
MeqdadDev <meqdad.darweesh@gmail.com>
2023-09-09 15:23:09 +0300

    add backend service for weather data table

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -20,7 +20,7 @@ def valid_login(email, password):
         auth_service.sign_in_with_email_and_password(email, password)
         login_visit = True
         return email, password
-    except:
+    except Exception:
         return False
 
 
@@ -41,31 +41,37 @@ def charts():
 @app.route("/tables")
 def tables():
     i = 0
-    # firebase_items = db.child("Raspberry Pi Data").child("Ships").get()
-    firebase_items = db.child("Raspberry Pi Data").child(
-        "Ships").get()  # .limit_to_first(20).get()
+    ships_items = db.child("Raspberry Pi Data").child("Ships").get()
+    weather_items = db.child("Raspberry Pi Data").child("dht").get()
     ships = []
+    weather_data = []
     allowed_ships = ["Passenger Ship", "Fishing Ship"]
-    # oceanus_db.create_fake_data(2)
-    # for i in range(10):
-    for i in firebase_items.each():
-        print(i.val())
+    for i in ships_items.each():
+        # print(i.val())
         ship_item = i.val()
         ship_name = ship_item["name"].capitalize()
         ship_timestamp = ship_item["timestamp"]
-        # print("ship_name", ship_name)
-        # print("ship_timestamp", ship_timestamp)
-        # ship_name = str(i.val()["name"])
-        # name = str(i.val()["name"])
         ship_timestamp = str(datetime.fromtimestamp(ship_timestamp))
         date_object = datetime.strptime(ship_timestamp, "%Y-%m-%d %H:%M:%S.%f")
         formatted_date = date_object.strftime(
             "Date: %Y-%m-%d / Time: %H:%M:%S.%f")[:-4]
         ships.append({"name": f"{ship_name} Ship", "time": formatted_date})
 
-    # print("ships::::", ships[:20])
-    # return render_template("table.html", ships=ships[-20:], allowed_ships=allowed_ships)
-    return render_template("table.html", ships=ships[-20:], allowed_ships=allowed_ships)
+    for i in weather_items.each():
+        weather_item = i.val()
+        humidity = weather_item["humidity"]
+        temperature = weather_item["temperature"]
+        weather_timestamp = weather_item["timestamp"]
+        weather_timestamp = str(datetime.fromtimestamp(weather_timestamp))
+        date_object = datetime.strptime(
+            weather_timestamp, "%Y-%m-%d %H:%M:%S.%f")
+        formatted_date = date_object.strftime(
+            "Date: %Y-%m-%d / Time: %H:%M:%S.%f")[:-4]
+        weather_data.append({"humidity": humidity,
+                             "temperature": temperature,
+                             "time": formatted_date})
+
+    return render_template("table.html", dhts=weather_data[-20:], allowed_ships=allowed_ships, ships=ships[-20:])
 
 
 @app.route("/all_ships")
