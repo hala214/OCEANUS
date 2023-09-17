1561239c1a99d62db3a259b7da5b08bd7d55a628
Zainamusleh <zainamusleh09@gmail.com>
2023-09-17 19:45:40 +0300

    the final products

--- Dashboard/section.py
+++
@@ -1,4 +0,0 @@
-x= input ('enter the width')
-d= input ('enter the length')
-y= input ('enter the height')
-area= int(x)*2*int(y)+int(d)
--- Dashboard/section3.py
+++
@@ -1,2 +0,0 @@
-x=input('enter your number')
-print (int(x[0])+int(x[1]))
